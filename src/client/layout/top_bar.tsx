17f9d469135cff27577f3154258e777fb2f0050c
ryannd <ryan2dimaranan@gmail.com>
2022-06-02 22:48:23 -0500

    hotfix header title

--- src/client/layout/top_bar.tsx
+++ src/client/layout/top_bar.tsx
@@ -1,13 +1,27 @@
-import { Header, Group, Space, Title } from '@mantine/core';
+import {
+  Header,
+  Group,
+  Space,
+  Title,
+  useMantineColorScheme,
+} from '@mantine/core';
 import { BsMusicNoteList } from 'react-icons/bs';
 import MenuLoggedIn from '~client/layout/menu_logged_in';
 import MenuLoggedOut from './menu_logged_out';
 
 const TopBar = ({ data }) => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
   return (
     <Header height={60} fixed>
       <Group sx={{ height: '100%' }} px={25} position="apart">
-        <a href="/">
+        <a
+          href="/"
+          style={{
+            textDecoration: 'none',
+            color: `${dark ? 'white' : 'black'}`,
+          }}
+        >
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <BsMusicNoteList />
             <Space w="md" />
